69f0de6eb9d702587aeccf94730cd3e59f27477a
Joselo Mantilla <mantilla.carlosjoselo@gmail.com>
2022-06-04 23:15:09 -0500

    More concepts and examples

--- index.js
+++ index.js
@@ -62,4 +62,114 @@ arrVacio.push(segundoObjeto);
 console.log(arr);
 console.log(arrVacio);
 
-//
+//_______________________________
+// -----------> Operadores
+// Los operadores son los mismos que existen en la vida real
+// Suma +
+const suma = 1 + 2;
+// Restar -
+const restar = 1 - 2;
+//Multitplicar
+const multiplicar = 1 * 2;
+//Dividir
+const dividir = 4 / 2;
+//Modulo
+const modulo = 10 % 3;
+//Se puede sumar o mezclar los operadores
+//Aumento y decremento (++) y (--)
+//Aumenta en 1
+let num = 5;
+num++;
+
+// -----> Operadores de comparacion
+//Sirven para comparar entre datos
+const resultado1 = 5 === 5; //Igualdad estricta
+const resultado2 = 5 == 6; //Igualdad no estricta
+//Se puede usar mayor que, menor que, menor igual o mayor igual
+const resultado3 = 5 < 6; //Menor que
+//Desigualdad estricta
+const resultado4 = 5 !== 6; //DesIgualdad estricta
+const resultado5 = 5 !== 6; //DesIgualdad no estricta
+
+// ------> Operadores Logicos
+//and &&, or ||, not!
+//Pueden tener mas condiciones
+const resOr = false || true; // Uno de los dos tiee que ser verdadero
+const reAnd = true && true; // Los dos tienen que ser verdadero
+
+//__________________________________
+// ---> Control de flujo
+// Nos permite realizar iteraciones o evaluar condiciones
+// Control de flujo if y else
+if (true) {
+  console.log("Estoy dentro de un if");
+}
+
+// Control de flujo while
+// Itera hasta el final del ciclo
+let x = 0;
+while (x < 5) {
+  console.log(x);
+  x++;
+}
+
+//Control de flujo switch
+// Nos ayuda a ejecutar una ejecucion u otra
+//Estoe valua dependiendo el caso
+let key = 1;
+switch (key) {
+  case 1:
+    console.log("Hola Mundo");
+    break;
+
+  default:
+    console.log("No es el uno");
+    break;
+}
+
+// Control de flujo for
+// Es el mas usado
+console.log("Este es un for");
+for (let index = 0; index < 10; index++) {
+  console.log(index);
+}
+
+//__________________________
+// ----> Funciones
+//Introduccion
+console.log("Funciones ----->");
+function miFuncion(arg1) {
+  for (let index = 0; index < arg1.length; index++) {
+    const element = arg1[index];
+    console.log(element);
+  }
+}
+const numeros = [1, 2, "Hola", 3, 4];
+const nombres = ["Juna", "Ricardo"];
+
+miFuncion(numeros);
+miFuncion(nombres);
+
+function sumaNumero(a, b) {
+  // console.log(a + b);
+  return a + b;
+}
+const resul = sumaNumero(3, 5);
+console.log(resul);
+
+// Callbacks
+// Funcion que se ejecuta al final de una funcion
+//Las funciones pueden ser valores y se pueden pasar como argumentos a otras funciones
+function sum(a, b, cb) {
+  const res = a + b;
+  cb(res);
+}
+function callback(r) {
+  console.log(r);
+}
+sum(3, 4, callback);
+
+// Fat arrow fuction
+//No se usa function
+//Se puede hacer en una sola linea y no hace falta el return
+const arrowFunction = (a, b) => a + b;
